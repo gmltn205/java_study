c889b46aefb1991dbebadea2fd18147b728612c9
gmltn205 <74577657+gmltn205@users.noreply.github.com>
2023-09-19 14:08:46 +0900

    j

---
+++ GooGooDan.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GooGooDan {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        sc.close();
+        int i;
+        for (i = 0; i <= 9; i++) {
+            System.out.println(number + " * " + i + "=" + number * i);
+        }
+
+    }
+}
